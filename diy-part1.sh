4618e7ecce1bb6c5f1688a8732a4c19e9f5a8280
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-12-08 15:15:57 +0800

    p

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,9 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+
+rm -rf package/lean/luci-theme-argon
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
@@ -27,4 +30,4 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #add packages
 #sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdn
-rm -rf package/lean/luci-theme-argon
+
