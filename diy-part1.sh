4e9188fb50776149f65f2ef8c360178d3b857a75
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-12-08 20:58:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,19 +13,19 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 
-rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 
 #Clone VSSR source code
-sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+#sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
 
 #add openwrt-packages
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 #add packages
 #sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
